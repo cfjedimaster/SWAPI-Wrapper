90b696694b0acf454ec7a9432283f82bae48d621
Raymond Camden <raymondcamden@gmail.com>
2014-12-31 17:29:25 -0600

    updates for v2

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'lib/swapi.js',
+        dest: 'lib/swapi.min.js'
+      }
+    },
+    jshint: {
+      all: ['lib/swapi.js']
+    },
+    jasmine: {
+      all: {
+        src:"lib/swapi.js",
+        options: {
+          specs:"tests/spec/swapiSpec.js",
+          '--web-security':false
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  
+  grunt.registerTask('default', ['jasmine','jshint','uglify']);
+
+};
